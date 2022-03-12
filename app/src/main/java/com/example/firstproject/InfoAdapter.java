95445b978f6c6d30ae001e272167c1e253a63949
fanthasium <anhosung00@gmail.com>
2022-03-13 01:46:03 +0900

    리사이클러뷰 with CardView

---
+++ app/src/main/java/com/example/firstproject/InfoAdapter.java
@@ -0,0 +1,13 @@
+package com.example.firstproject;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class InfoAdapter extends RecyclerView.ViewHolder {
+
+    public InfoAdapter(@NonNull View itemView) {
+        super(itemView);  //데이터 바인딩 사용
+    }
+}
--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableArrayList;
+
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+
+import com.example.firstproject.databinding.ActivityIdPagBinding;
+import com.example.firstproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
 
+     ActivityMainBinding mbinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mbinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mbinding.setMain(this);
     }
+
+
 }
